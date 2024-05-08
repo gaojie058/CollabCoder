ce2c57e8ef5d01656466c38d7b3e546e07795311
John Doe <johndoe@example.com>
2024-05-08 20:08:50 +0800

    finish FAQ

--- _site/assets/data/FAQdata.js
+++ _site/assets/data/FAQdata.js
@@ -1,10 +1,10 @@
 var data = [
     {
-        question: "What is the purpose of this website?",
-        answer: "This website is designed to provide a platform for students to share their study tips and resources, as well as to connect with other students who are facing similar challenges. By providing a centralized platform, we hope to help students achieve their academic goals more efficiently and effectively."
+        question: "What if we found bugs in installation?",
+        answer: "Reporting an issue on GitHub!"
     },
     {
-        question: "How can I contribute to the website?",
-        answer: "You can contribute to the website by submitting study tips and resources that you think will be helpful to other students. You can also suggest new features or report bugs on the website. Your contributions are greatly appreciated and will help us continue to improve the website for the benefit of all students."
+        question: "I have suggestions for the design or function of CollabCoder!",
+        answer: "Write your comment to https://github.com/gaojie058/CollabCoder_system/issues/2"
     }
 ]
